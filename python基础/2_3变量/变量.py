96e6509ec7c05512113edfa5d0facc9d2a93436b
game606 <309965046@qq.com>
2026-07-22 18:32:27 +0800

    2.3 变量

---
+++ python基础/2_3变量/变量.py
@@ -0,0 +1,28 @@
+"""
+    只能由字母、数字、下划线组成，且不能以数字开头；
+    不能用 Python 关键字（比如if、print、True）；
+    区分大小写（name和Name是两个不同变量）；
+    建议见名知意（比如用age存年龄，不用a1）。
+"""
+
+Age = 18
+
+age = 15
+
+print(Age)
+print(age)
+
+print(age + 220)
+
+age = 110
+print(age)
+
+age = age + 5
+print(age)
+
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+print(type(age))
