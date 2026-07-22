b85c701ea879c4e373ee2ad2614585816b387a82
game606 <309965046@qq.com>
2026-07-22 18:11:50 +0800

    2.1 字面量

---
+++ python基础/字面量/字面量.py
@@ -0,0 +1,30 @@
+# 123
+# -45
+# 0
+# 3.14
+# -0.5
+# 2+3j
+#
+# 'Hello'
+# "World"
+# '''
+# 第一行
+# 第二行
+# '''
+#
+# True
+# False
+# None
+
+# 数字
+print(123)       # 输出123
+print(3.14)      # 输出3.14
+
+# 字符串
+print('python')  # 输出python
+
+# 布尔值
+print(True)      # 输出True
+
+# 空值
+print(None)      # 输出None
